b98e956b94490884d1934a22749df1cd8bbaa046
Yoniher Meléndez Caicedo <yoniher.melendez@mvm.com.co>
2023-02-20 15:33:49 -0500

    Se corre el programa de manera correcta

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
+//Variables CONSTANTES
+const val MES = "Enero"
+
 fun main() {
     println("Hola Mundo")
+
+    // Definicion de variables de lectura y escritura
+    var dinero = 0
+
+    // Definicion de variables de solo lectura
+    val nombre = "Luis Mario"
+
+    println("El valor de las variables es: $nombre valor constante: $MES")
+
+    // Variables Booleanas
+    var boolean = true
+    // Variable numerica grande - var numerolargo = 12345678901L -> definicion abreviada
+    var numerolargo : Long = 12345678901
+    // Variables decimales
+    var numeroDecimal : Double = 2.5
+    var numeroDecimalMayor : Float = 2.5F
+
 }
