30496912048529dd6c9e870edbadd5c9777f6c7a
Yoniher Meléndez Caicedo <yoniher.melendez@mvm.com.co>
2023-02-22 12:35:10 -0500

    Se añade clase sobre ordenamiento de listas

--- src/main/kotlin/Listas.kt
+++ src/main/kotlin/Listas.kt
@@ -12,4 +12,31 @@ fun main() {
     //Obtencion de datos
     println(listaVacia[0])
     println(listaVacia.get(1))
+
+    val numerosLoteria = listOf<Int>(1,2,46,78,53,89)
+
+    // Ordenar lista de manera ascendente
+    numerosLoteria.sorted();
+    println(numerosLoteria)
+
+    // Orden desendente
+    val numeroDesendentes =  numerosLoteria.sortedDescending()
+    println(numeroDesendentes)
+
+    // Ordenar lista con una condicion
+    numerosLoteria.sortedBy { numeros -> numeros < 30 }
+    println(numerosLoteria)
+
+    // Orden reversiva
+    val numerosLoteriaReverse = numerosLoteria.reversed()
+    println(numerosLoteriaReverse)
+
+    // Convertir lista de enteros a cadena de texto con map mediante programacion funcional
+    val listanumeros2 = listOf(10,20,30,40,50)
+    val listaConversion = listanumeros2.map { numeros -> "El numero es: $numeros" }
+    println(listaConversion)
+
+    val numerosFiltrados = listanumeros2.filter { numeros -> numeros < 30 }
+    println(numerosFiltrados)
+
 }
