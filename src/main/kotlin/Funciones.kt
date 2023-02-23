c0ec8ef40118d13dddbfe9048d9dbfb84290881f
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-02-22 23:20:18 -0500

    Se añade clase sobre las funciones y funciones ded extencion

---
+++ src/main/kotlin/Funciones.kt
@@ -0,0 +1,34 @@
+fun main() {
+    println("La suma de los numeros es: "+sumaDeNumeros(40,40))
+    imprimir()
+    println(ramdomCase("Hola mundo"))
+    println("Hola".imprimirMensaje())
+}
+
+// Funcion que retorna valores
+fun sumaDeNumeros(numero1: Int, numero2 : Int): Int {
+    return numero1 + numero2
+}
+
+// Funcion que no retorna
+fun imprimir(): Unit {
+    println("Otra suma de numeros es: "+1+2)
+}
+
+//Funcion
+fun ramdomCase(frase : String) : String{
+    val numeros = 0..99
+    val numeroAleatorio = numeros.random()
+
+    return if (numeroAleatorio.rem(2) == 0)  frase.uppercase() else  frase.lowercase()
+}
+
+
+// Funciones de extencion
+fun String.imprimirMensaje (): String {
+    if (this.length > 10)
+        return "mensaje mayor"
+    else{
+        return "mensaje menor"
+    }
+}
