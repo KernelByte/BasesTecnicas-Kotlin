49f915e4cbc3d79819ead8c278a9716e87c5953b
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-02-24 19:56:42 -0500

    Se da clase sobre WITH para ver valores de varias variables

--- src/main/kotlin/With.kt
+++ src/main/kotlin/With.kt
@@ -1,3 +1,9 @@
 fun main() {
-    
+
+    // Ver facilmente los valores de una variable
+    val listaFrutas = listOf("Manzana","Pera","Mango")
+    with(listaFrutas) {
+        println("La lista de frutas es: $this")
+        println("El total de frutas es: $size")
+    }
 }
